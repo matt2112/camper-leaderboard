9b06e62acd18adbe513714aac706b2f5f8287d28
matt2112 <matt_lewis@live.co.uk>
2016-08-10 14:15:08 +0100

    begin attempt to use react-http-request

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -1,14 +1,26 @@
 import React from 'react';
-
+import Data from './Data';
 import Table from './Table';
 
 export default class Layout extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            focus: "thirty-days"
+        };
+    }
+
+    changeFocus(focus) {
+        this.setState({focus});
+    }
 
     render() {
         return (
             <div id="main">
                 <h1 id="title">freeCodeCamp Leaderboard</h1>
-                <Table />
+                <Table changeFocus={this.changeFocus.bind(this)} />
+                {/* <Data /> */}
+                <h2>{this.state.focus}</h2>
             </div>
         );
     }
--- src/js/components/Table.js
+++ src/js/components/Table.js
@@ -1,28 +1,36 @@
 import React from "react";
 
 export default class Table extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            title: "Welcome",
-        };
+
+    changeToThirty() {
+        const focus = "thirty-days";
+        this.props.changeFocus(focus);
+    }
+
+    changeToAll() {
+        const focus = "all-time";
+        this.props.changeFocus(focus);
     }
 
     render() {
         return (
             <table id="leaderboard">
-                <tr>
-                    <th>#</th>
-                    <th>Camper Name</th>
-                    <th>Points in past 30 days</th>
-                    <th>All time points</th>
-                </tr>
-                <tr>
-                    <td>one</td>
-                    <td>two</td>
-                    <td>three</td>
-                    <td>four</td>
-                </tr>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Camper Name</th>
+                        <th onClick={this.changeToThirty.bind(this)}>Points in past 30 days</th>
+                        <th onClick={this.changeToAll.bind(this)}>All time points</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>one</td>
+                        <td>two</td>
+                        <td>three</td>
+                        <td>four</td>
+                    </tr>
+                </tbody>
             </table>
         );
     }
