b40170b1a7755b56001a368eda1c496067f5fa1e
matt2112 <matt_lewis@live.co.uk>
2016-08-20 12:37:30 +0100

    finish TableHead component

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -1,14 +1,71 @@
 import React, { Component } from 'react';
-import Table from './Table';
+import Axios from 'axios';
 
-export default class Layout extends Component {
+import TableHead from './TableHead';
+
+class Table extends Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            data: []
+        };
+
+        this.getData("all-time");
+    }
+
+    getData(focus) {
+        var url = "https://fcctop100.herokuapp.com/api/fccusers/top/";
+        if (focus === "thirty-days") {
+            url += "recent";
+        } else if (focus === "all-time") {
+            url += "alltime";
+        }
+        Axios
+            .get(url)
+            .then(result => {
+                this.setState({ data: result });
+            });
+    }
 
     render() {
         return (
             <div id="main">
                 <h1 id="title">freeCodeCamp Leaderboard</h1>
-                <Table />
+                <table id="leaderboard">
+                    <TableHead getData={this.getData.bind(this) } />
+
+                </table>
             </div>
         );
     }
 }
+
+export default Table;
+
+// <thead>
+//     <tr>
+//         <th>#</th>
+//         <th>Camper Name</th>
+//         <th>Points in past 30 days
+//             <span className="sortButton" onClick={this.getData.bind(this, "thirty-days") }></span>
+//         </th>
+//         <th>All time points
+//             <span className="sortButton" onClick={this.getData.bind(this, "all-time") }></span>
+//         </th>
+//     </tr>
+// </thead>
+
+// <tbody>
+//     {this.state.data.map(function (user, idx) {
+//         return (
+//             <tr key={idx} className={"row" + (idx + 1) } >
+//                 <td>{idx + 1}</td>
+//                 <td className="camper"><img className="thumbnail" src={user.img} /> <span className="username">{user.username}</span></td>
+//                 <td>{user.recent}</td>
+//                 <td>{user.alltime}</td>
+//             </tr>
+//         );
+//     }) }
+// </tbody>
--- src/js/components/TableHead.js
+++ src/js/components/TableHead.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const TableHead = ({getData}) => {
-    console.log(getData);
     return (
         <thead>
             <tr>
@@ -11,7 +10,7 @@ const TableHead = ({getData}) => {
                     <span className="sortButton" onClick={ () => getData("thirty-days") }></span>
                 </th>
                 <th>All time points
-                    <span className="sortButton" onClick={ console.log("nya2") }></span>
+                    <span className="sortButton" onClick={ () => getData("all-time") }></span>
                 </th>
             </tr>
         </thead>
